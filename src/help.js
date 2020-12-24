884246cbb199a4bab1bd9306f56354b4c36284c7
Yoshi0213 <74713036+Yoshi0213@users.noreply.github.com>
2020-12-24 23:44:06 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,40 @@
+const help = (prefix) => {
+	return `*>- YOBOTS Self Commands -<*_
+
+```INFORMATIONS```
+┌ *➪Library : Baileys*
+├ *➪Running : ${pad(hours)} H,${pad(minutes)} M,${pad(seconds)} S*
+└ *➪Support : Ipul , Shirayuki , Dll*
+
+```SELF SYSTEM```
+┌ *➪ ${prefix}help*
+└ *➪ ${prefix}setprefix*
+
+```SELF GROUP```
+┌ *➪ ${prefix}hidetag*
+├ *➪ ${prefix}tagall*
+├ *➪ ${prefix}runtime*
+├ *➪ ${prefix}linkgroup*
+└ *➪ ${prefix}crp*
+
+```SELF MEDIA```
+┌ *➪ ${prefix}sticker*
+├ *➪ ${prefix}sticker nobg*
+├ *➪ ${prefix}toimg*
+├ *➪ ${prefix}tsticker*
+├ *➪ ${prefix}meme*
+├ *➪ ${prefix}memeindo*
+├ *➪ ${prefix}gtts*
+├ *➪ ${prefix}loli*
+├ *➪ ${prefix}nsfwloli*
+├ *➪ ${prefix}url2img*
+├ *➪ ${prefix}simi*
+├ *➪ ${prefix}ocr*
+├ *➪ ${prefix}wait*
+└ *➪ ${prefix}simih*
+
+```MORE FITURE IS COMMING```
+`
+}
+
+exports.help = help
